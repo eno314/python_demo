4e7aae479c4a38d0d9e81680491010505d0dfc4f
Hiroto Kitamura <kirikonknow@gmail.com>
2023-01-22 14:56:00 +0900

    add performance test.

--- tests/latest_and_its_prev_and_next_date/test_usecase.py
+++ tests/latest_and_its_prev_and_next_date/test_usecase.py
@@ -126,7 +126,7 @@ class Test_find_latest_and_its_prev_and_next_date_v2:
 class Test_performance:
 
     def test_performance_v1(self, benchmark):
-        sut = self._setup_usecase_having_demo_repository_with_many_data()
+        sut = self._setup_usecase_having_demo_repository_with_many_data(1)
         result = benchmark(
             sut.find_latest_and_its_prev_and_next_date_v1,
             date(2023, 12, 31)
@@ -134,16 +134,64 @@ class Test_performance:
         assert result
 
     def test_performance_v2(self, benchmark):
-        sut = self._setup_usecase_having_demo_repository_with_many_data()
+        sut = self._setup_usecase_having_demo_repository_with_many_data(1)
         result = benchmark(
             sut.find_latest_and_its_prev_and_next_date_v2,
             date(2023, 12, 31)
         )
         assert result
 
-    def _setup_usecase_having_demo_repository_with_many_data(self):
+    def test_performance_10times_v1(self, benchmark):
+        sut = self._setup_usecase_having_demo_repository_with_many_data(10)
+        result = benchmark(
+            sut.find_latest_and_its_prev_and_next_date_v1,
+            date(2023, 12, 31)
+        )
+        assert result
+
+    def test_performance_10times_v2(self, benchmark):
+        sut = self._setup_usecase_having_demo_repository_with_many_data(10)
+        result = benchmark(
+            sut.find_latest_and_its_prev_and_next_date_v2,
+            date(2023, 12, 31)
+        )
+        assert result
+
+    def test_performance_100times_v1(self, benchmark):
+        sut = self._setup_usecase_having_demo_repository_with_many_data(100)
+        result = benchmark(
+            sut.find_latest_and_its_prev_and_next_date_v1,
+            date(2023, 12, 31)
+        )
+        assert result
+
+    def test_performance_100times_v2(self, benchmark):
+        sut = self._setup_usecase_having_demo_repository_with_many_data(100)
+        result = benchmark(
+            sut.find_latest_and_its_prev_and_next_date_v2,
+            date(2023, 12, 31)
+        )
+        assert result
+
+    def test_performance_1000times_v1(self, benchmark):
+        sut = self._setup_usecase_having_demo_repository_with_many_data(1000)
+        result = benchmark(
+            sut.find_latest_and_its_prev_and_next_date_v1,
+            date(2023, 12, 31)
+        )
+        assert result
+
+    def test_performance_1000times_v2(self, benchmark):
+        sut = self._setup_usecase_having_demo_repository_with_many_data(1000)
+        result = benchmark(
+            sut.find_latest_and_its_prev_and_next_date_v2,
+            date(2023, 12, 31)
+        )
+        assert result
+
+    def _setup_usecase_having_demo_repository_with_many_data(self, size):
         repository = DemoRepository()
         date_list = [date(2023, 1, 1) + timedelta(days=i) for i in range(365)]
-        for _ in range(1000):
+        for _ in range(size):
             repository.add_dates(date_list)
         return Usecase(repository)
