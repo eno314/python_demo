e91eac0b0fea239bc52615c90d50abf2fb339080
Hiroto Kitamura <kirikonknow@gmail.com>
2025-10-26 17:27:10 +0900

    refactor: improve account validation and update method names in Bank class

--- demo/simple_bank_system/bank.py
+++ demo/simple_bank_system/bank.py
@@ -8,18 +8,21 @@ class Bank:
 
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        return self.withdraw(account1, money) and self.deposit(account2, money)
-
+        if self._is_valid_account(account1) and self._is_valid_account(account2):
+            return self.withdraw(account1, money) and self.deposit(account2, money)
+        return False
 
     def deposit(self, account: int, money: int) -> bool:
-        return self.update_balance(account, money)
-
+        return self._update_balance(account, money)
 
     def withdraw(self, account: int, money: int) -> bool:
-        return self.update_balance(account, -money)
+        return self._update_balance(account, -money)
 
-    def update_balance(self, account: int, diff: int) -> bool:
-        if (account < 1 or account > len(self.balance)):
+    def _is_valid_account(self, account: int) -> bool:
+        return 1 <= account <= len(self.balance)
+
+    def _update_balance(self, account: int, diff: int) -> bool:
+        if not self._is_valid_account(account):
             return False
         if (diff < 0 and self.balance[account - 1] + diff < 0):
             return False
